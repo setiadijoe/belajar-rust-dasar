fc4a6d5ea1c3c8860b8df20db4036e576d5ea2a1
Yonathan Setiadi <yonathan.setiadi@sicepat.com>
2025-04-09 17:29:53 +0700

    learning rust until option enum

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+mod first;
+mod second;
+mod model;
+mod third;
+
+use first::hello;
+use second::hello as hello_second;
+use model::User;
+
+#[test]
+fn test_module() {
+    let user = User::create_person(
+        String::from("Yonathan"), 
+        String::from("Setiadi"),
+        String::from("nathan"), 
+        String::from("nathan@mail.com"), 
+        32);
+
+    user.say_hello("Arif");
+}
+
+
+
+
+#[test]
+fn test_use() {
+    hello();
+    hello_second();
+    first::second::third::hello();
+}
+
 fn main() {
     println!("Hello, world!");
 }
@@ -246,19 +277,575 @@ fn test_hi() {
     println!("My name {}", name);
 }
 
-fn fullname(first: String, last: String) -> (String, String, String) {
-    let full_name = format!("{} {}", first, last);
-
-    (first, last, full_name)
+fn fullname(first: &String, last: &String) -> String {
+    format!("{} {}", first, last)
 }
 
 #[test]
 fn test_fullname() {
     let first = String::from("Yonathan");
     let last = String::from("Setiadi");
-    let (first, last, name) = fullname(first, last);
+    let fullname = fullname(&first, &last);
 
-    println!("{}", name);
-    println!("{}", first);
-    println!("{}", last);
+    println!("{}", fullname);
 }
+
+fn change_value(value: &mut String) {
+    value.push_str("Test");
+}
+
+#[test]
+fn test_change_value() {
+    let mut value = String::from("Makanan");
+    change_value(&mut value);
+    println!("{}", value);
+}
+
+fn get_fullname(first: &String, last: &String) -> String {
+    format!("{} {}", first, last)
+}
+
+#[test]
+fn test_get_fullname() {
+    let first = String::from("Yonathan");
+    let last = String::from("Setiadi");
+    let fullname = get_fullname(&first, &last);
+
+    println!("{}", fullname);
+}
+
+#[test]
+fn slice_references() {
+    let array: [i32;10]= [0,1,2,3,4,5,6,7,8,9];
+    let full: &[i32] = &array[..];
+    println!("{:?}", full);
+
+    let part_1: &[i32] = &array[..5];
+    println!("{:?}", part_1);
+
+    let part_2: &[i32] = &array[6..];
+    println!("{:?}", part_2);
+
+}
+
+#[test]
+fn string_slice() {
+    let name = String::from("Yonathan Setiadi");
+    let first_name: &str = &name[..8];
+    println!("{}", first_name);
+}
+
+struct Person {
+    first_name: String,
+    last_name: String,
+    age: u8
+}
+
+#[test]
+fn test_struct_person() {
+    let first_name = String::from("Yonathan");
+    let person = Person{
+        age: 3,
+        first_name,
+        last_name : String::from("Setiadi"),
+    };
+
+    let person2 = Person{
+        first_name: person.first_name.clone(),
+        last_name: person.last_name.clone(),
+        ..person
+    };
+
+    print_person(&person2);
+    print_person(&person);
+}
+
+fn print_person(person: &Person) {
+    println!("{}", person.first_name);
+    println!("{}", person.last_name);
+    println!("{}", person.age);
+}
+
+struct GeoPoint(f64, f64);
+
+#[test]
+fn test_geopoint() {
+    let geopoint = GeoPoint(-3.00, 4.22);
+
+    println!("{}", geopoint.0);
+    println!("{}", geopoint.1);
+}
+
+impl Person {
+    fn say_hello(&self, name: &str) {
+        println!("Hello {}, my name is {} ",name, self.first_name);
+    }
+}
+
+#[test]
+fn test_method() {
+    let person = Person{
+        age: 3,
+        first_name: String::from("Yonathan"),
+        last_name : String::from("Setiadi"),
+    };
+
+    person.say_hello("Arif");
+    println!("{}", person.first_name);
+}
+
+impl GeoPoint {
+    fn new(long: f64, lat: f64) ->GeoPoint {
+        GeoPoint(long, lat)
+    }
+}
+
+#[test]
+fn test_assosiated_function() {
+    let geo_point = GeoPoint::new(0.78, -8.99);
+    println!("{}", geo_point.0);
+}
+
+enum Level {
+    Regular,
+    Premium,
+    Platinum,
+}
+
+enum Payment {
+    CreditCard(String),
+    BankTransfer(String, String),
+    EWallet(String, String),
+}
+
+impl Payment {
+    fn pay(&self, amount: u32) {
+        match self {
+            Payment::CreditCard(number) => {
+                println!("Paying with credit card {} amount {}", number, amount);
+            }
+            Payment::BankTransfer(bank, number) => {
+                println!("Paying with bank {} and number {}, amount {}", bank, number, amount);
+            }
+            Payment::EWallet(wallet,number ) => {
+                println!("Paying with wallet {} and number {}, amount {}", wallet, number, amount);
+            }
+        }
+    }
+}
+
+#[test]
+fn test_payment() {
+    let payment = Payment::BankTransfer(String::from("BCA"), String::from("8326297364324"));
+    payment.pay(3000);
+    let payment1 = Payment::EWallet(String::from("Gopay"), String::from("8326297364324"));
+    payment1.pay(67898);
+    let payment2 = Payment::CreditCard(String::from("8326297364324"));
+    payment2.pay(67898);
+}
+
+#[test]
+fn test_enum() {
+    let level = Level::Premium;
+
+    match level {
+        Level::Regular => {
+            println!("Regular");
+        }
+        Level::Premium => {
+            println!("Premium");
+        }
+        Level::Platinum => {
+            println!("Platinum");
+        }
+    }
+}
+
+#[test]
+fn test_match_value() {
+    let name = "Amin";
+
+    match name {
+        "Eko" | "Budi"=> {
+            println!("Hello Eko!");
+        }
+        "Joko" => {
+            println!("Hello Budi");
+        }
+        other => {
+            println!("Bangun kau {}!", other);
+        }
+    }
+}
+
+#[test]
+fn test_range_pattern() {
+    let value = 75;
+
+    match  value {
+        75..=100 => {
+            println!("Great!");
+        }
+        60..75 => {
+            println!("Good!");
+        }
+        40..=59 => {
+            println!("Bad!");
+        }
+        0..40 => {
+            println!("Too Bad");
+        }
+        other => {
+            println!("Not a valid number {}", other);
+        }
+    }
+
+    let person = Person{
+        first_name: String::from("Budi"),
+        last_name: String::from("Karya"),
+        age: 8,
+    };
+
+    match  person {
+        Person{first_name, age, ..} => {
+            println!("Name {} age {} ", first_name, age);
+        }
+    }
+
+}
+
+// Type Alias
+type Age = u8;
+type IdentityNumber = String;
+
+struct Costumer {
+    name: String,
+    age: Age,
+    id_number: IdentityNumber,
+}
+#[test]
+fn test_customer() {
+    let customer = Costumer{
+        name: String::from("Yonathan"),
+        age: 32,
+        id_number: String::from("1234567890"),
+    };
+
+    println!("Name: {}", customer.name);
+    println!("Age: {}", customer.age);
+    println!("ID Number: {}", customer.id_number);
+}
+
+trait CanSayHello {
+    fn hello(&self) -> String{
+        String::from("Hello")
+    }
+    fn say_hello(&self) -> String;
+    fn say_hello_to(&self, name: &str) -> String;
+}
+
+trait CanSayGoodbye {
+    fn say_goodbye(&self) -> String;
+    fn say_goodbye_to(&self, name: &str) -> String;
+}
+
+impl CanSayHello for Person {
+    fn say_hello(&self) -> String {
+        format!("Hello, my name is {}", self.first_name)
+    }
+
+    fn say_hello_to(&self, name: &str) -> String {
+        format!("Hello {}, my name is {}", name, self.first_name)
+    }
+}
+
+impl CanSayGoodbye for Person {
+    fn say_goodbye(&self) -> String {
+        format!("Goodbye, my name is {}", self.first_name)
+    }
+
+    fn say_goodbye_to(&self, name: &str) -> String {
+        format!("Goodbye {}, my name is {}", name, self.first_name)
+    }
+}
+
+fn can_say_hello(person: &impl CanSayHello) {
+    println!("{}", person.say_hello());
+}
+
+fn say_hello_goodbye(value: &(impl CanSayHello + CanSayGoodbye)) {
+    println!("{}", value.say_hello());
+    println!("{}", value.say_goodbye());
+}
+
+#[test]
+fn test_trait() {
+    let person = Person{
+        first_name: String::from("Yonathan"),
+        last_name: String::from("Setiadi"),
+        age: 32,
+    };
+
+    can_say_hello(&person);
+
+    println!("{}", person.say_hello_to("Arif"));
+    println!("{}", person.hello());
+    println!("{}", person.say_goodbye());
+    println!("{}", person.say_goodbye_to("Arif"));
+
+    say_hello_goodbye(&person);
+
+    CanSayHello::say_hello(&person);
+    Person::say_hello(&person, "Arif");
+}
+
+struct SimplePerson {
+    name: String
+}
+
+impl CanSayHello for SimplePerson {
+    fn say_hello(&self) -> String {
+        format!("Hello, my name is {}", self.name)
+    }
+
+    fn say_hello_to(&self, name: &str) -> String {
+        format!("Hello {}, my name is {}", name, self.name)
+    }
+}
+
+fn create_simple_person(name: String) -> impl CanSayHello {
+    SimplePerson{ name }
+}
+
+#[test]
+fn test_return_trait() {
+    let person = create_simple_person(String::from("Yonathan"));
+    println!("{}", person.say_hello());
+    println!("{}", person.say_hello_to("Arif"));
+}
+
+trait CanSay: CanSayHello + CanSayGoodbye {
+    fn say(&self);
+}
+
+struct SimpleMan {
+    name: String
+}
+
+impl CanSay for SimpleMan {
+    fn say(&self) {
+        println!("{}", self.say_hello());
+        println!("{}", self.say_goodbye());
+    }
+}
+
+impl CanSayHello for SimpleMan {
+    fn say_hello(&self) -> String {
+        format!("Hello, my name is {}", self.name)
+    }
+
+    fn say_hello_to(&self, name: &str) -> String {
+        format!("Hello {}, my name is {}", name, self.name)
+    }
+}
+
+impl CanSayGoodbye for SimpleMan {
+    fn say_goodbye(&self) -> String {
+        format!("Goodbye, my name is {}", self.name)
+    }
+
+    fn say_goodbye_to(&self, name: &str) -> String {
+        format!("Goodbye {}, my name is {}", name, self.name)
+    }
+}
+
+#[test] 
+fn test_trait_object() {
+    let person = SimpleMan{
+        name: String::from("Yonathan"),
+    };
+
+    person.say();
+    println!("{}", person.say_hello());
+    println!("{}", person.say_goodbye());
+}
+
+struct Point<T = i32> {
+    x: T,
+    y: T,
+}
+
+#[test]
+fn test_generic_struct() {
+    let point = Point::<i64>{
+        x: 10,
+        y: 20,
+    };
+
+    println!("x: {}, y: {}", point.x, point.y);
+
+    let point2 = Point::<f64>{
+        x: 10.5,
+        y: 20.5,
+    };
+
+    println!("x: {}, y: {}", point2.x, point2.y);
+}
+
+enum Value<T> {
+    NONE,
+    Value(T),
+}
+
+#[test]
+fn test_generic_enum() {
+    let value = Value::<i64>::Value(10);
+
+    match value {
+        Value::Value(v) => {
+            println!("Value: {}", v);
+        }
+        Value::NONE => {
+            println!("No Value");
+        }
+    }
+}
+
+struct Hi<T: CanSayHello> {
+    value: T,
+}
+
+#[test]
+fn test_generic_bound() {
+
+    let hi = Hi::<SimplePerson>{
+        value: SimplePerson { 
+            name: String::from("Yonathan")
+        }
+    };
+
+    println!("{}", hi.value.say_hello());
+}
+
+fn min<T: PartialOrd>(a: T, b: T) -> T {
+    if a < b {
+        a
+    } else {
+        b
+    }
+}
+
+#[test]
+fn test_generic_function() {
+    let a = 10;
+    let b = 20;
+
+    let min_value = min(a, b);
+    println!("Min: {}", min_value);
+
+    let c = 10.5;
+    let d = 20.5;
+
+    let min_value2 = min(c, d);
+    println!("Min: {}", min_value2);
+}
+
+impl<T> Point<T> {
+    fn new(x: T, y: T) -> Point<T> {
+        Point { x, y }
+    }
+
+    fn get_x(&self) -> &T {
+        &self.x
+    }
+
+    fn get_y(&self) -> &T {
+        &self.y
+    }
+}
+
+#[test]
+fn test_generic_method() {
+    let point = Point::new(10, 20);
+    println!("x: {}, y: {}", point.get_x(), point.get_y());
+
+    let point2 = Point::new(10.5, 20.5);
+    println!("x: {}, y: {}", point2.get_x(), point2.get_y());
+    println!("{}", point2.get_value());
+}
+
+trait GetValue<T> where T: PartialOrd {
+    fn get_value(&self) -> &T;
+}
+
+impl<T> GetValue<T> for Point<T> where T: PartialOrd {
+    fn get_value(&self) -> &T {
+        &self.x
+    }
+}
+
+// Overloadable Operator
+use core::ops::Add;
+use core::ops::AddAssign;
+
+struct Apple {
+    quantity: u32
+}
+
+impl Add for Apple {
+    type Output = Apple;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Apple {
+            quantity: self.quantity + rhs.quantity
+        }
+    }
+}
+
+impl AddAssign for Apple {
+    fn add_assign(&mut self, rhs: Self) {
+        self.quantity += rhs.quantity;
+    }
+}
+
+
+#[test]
+fn test_add() {
+    let apple1 = Apple { quantity: 10 };
+    let apple2 = Apple { quantity: 10 };
+
+    let apple3 = apple1 + apple2;
+    println!("Apple 3: {}", apple3.quantity);
+
+    let mut apple4 = Apple { quantity: 0 };
+    apple4 += apple3;
+    println!("Apple 4: {}", apple4.quantity);
+}
+
+// End Overloadable Operator
+
+// Option Enum
+
+
+fn double(value: Option<i32>) -> Option<i32> {
+    match value {
+        Some(v) => Some(v * 2),
+        None => None,
+    }
+}
+
+#[test]
+fn test_option() {
+    let value = Some(10);
+    let result = double(value);
+    println!("{:?}", result);
+
+    let value2 = None;
+    let result2 = double(value2);
+    println!("{:?}", result2);
+
+    let value3 = Some(20);
+    let result3 = double(value3);
+    println!("{:?}", result3);
+}
+// End Option Enum
